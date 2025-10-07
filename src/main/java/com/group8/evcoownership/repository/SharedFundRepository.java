4ac80cabad95d9a973187d8e39917c7ca269dbe4
KhanhVy <khanhvy0946265560@gmai.com>
2025-10-07 14:23:50 +0700

    feat: SharedFund fuction is processing, not done

--- src/main/java/com/group8/evcoownership/repository/SharedFundRepository.java
+++ src/main/java/com/group8/evcoownership/repository/SharedFundRepository.java
@@ -1,10 +1,12 @@
 package com.group8.evcoownership.repository;
 
 import com.group8.evcoownership.entity.SharedFund;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
-public interface SharedFundRepository {
+ @Repository
+public interface SharedFundRepository extends JpaRepository<SharedFund,Long> {
     Optional<SharedFund> findByGroup_GroupId(long group_GroupId);
     boolean existsByGroup_GroupId(long group_GroupId);
 }
