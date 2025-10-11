9e53208ce88bbe602dc9f9b511480bfa0e6f2c24
truonghoang <boybybame@gmail.com>
2025-10-11 14:07:51 +0700

    chore(config): replace hardcoded mail credentials with environment variables for Azure deployment

--- src/main/java/com/group8/evcoownership/EvCoOwnershipApplication.java
+++ src/main/java/com/group8/evcoownership/EvCoOwnershipApplication.java
@@ -1,6 +1,5 @@
 package com.group8.evcoownership;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EvCoOwnershipApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
         SpringApplication.run(EvCoOwnershipApplication.class, args);
     }
 }
