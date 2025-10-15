e16a5e917aa8983551ad898fffcc5b25c35527ba
khanhvy0908 <khanhvy0946265560@gmail.com>
2025-10-15 10:21:51 +0700

    fix repository Payment and Dispute

--- src/main/java/com/group8/evcoownership/repository/DisputeRepository.java
+++ src/main/java/com/group8/evcoownership/repository/DisputeRepository.java
@@ -4,7 +4,9 @@ import com.group8.evcoownership.entity.Dispute;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DisputeRepository extends JpaRepository<Dispute, Long> {
     Page<Dispute> findByStatusIgnoreCase(String status, Pageable pageable);
     Page<Dispute> findByFund_FundId(Long fundId, Pageable pageable);
