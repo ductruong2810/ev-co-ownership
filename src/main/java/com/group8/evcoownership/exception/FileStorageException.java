2374c0e217452c3f96c40ac641404f92e7adf8a0
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2025-10-16 15:47:59 +0700

    refactor: upload GPLX va CCCD

---
+++ src/main/java/com/group8/evcoownership/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.group8.evcoownership.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
