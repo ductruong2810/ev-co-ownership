89f62db328097b22d9749221f8a9f8ffeec16b05
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2025-10-23 15:38:05 +0700

    feat: custom error requestDTO

--- src/main/java/com/group8/evcoownership/controller/AuthController.java
+++ src/main/java/com/group8/evcoownership/controller/AuthController.java
@@ -7,8 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
