4edef5ed8f01969a08c21e370103c77828a692a5
truonghoang <boybybame@gmail.com>
2025-10-30 01:07:25 +0700

    fix: currency deposit send mail

--- src/main/java/com/group8/evcoownership/config/WebConfig.java
+++
@@ -1,8 +0,0 @@
-package com.group8.evcoownership.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig {
-    // CORS configuration is handled in SecurityConfig
-}
