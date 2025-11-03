d41e151d57b0742caa4930044f14ea466e4fdc22
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2025-11-03 22:54:52 +0700

    feat: add @JsonRawValue vao 2 properties checkin checkout

--- src/main/java/com/group8/evcoownership/dto/BookingQRCodeDTO.java
+++ src/main/java/com/group8/evcoownership/dto/BookingQRCodeDTO.java
@@ -1,5 +1,6 @@
 package com.group8.evcoownership.dto;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,9 @@ import lombok.NoArgsConstructor;
 public class BookingQRCodeDTO {
     private Long bookingId;
 //    private String qrCode;
+    @JsonRawValue
     private String qrCodeCheckin;
+    @JsonRawValue
     private String qrCodeCheckout;
     private String startDateTime;
     private String endDateTime;
