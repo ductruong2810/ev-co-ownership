bc247799a916579aa404023edde53a641a56a885
khanhvy0908 <khanhvy0946265560@gmail.com>
2025-11-07 15:55:09 +0700

    update SharedFund Repository

--- src/main/java/com/group8/evcoownership/repository/SharedFundRepository.java
+++ src/main/java/com/group8/evcoownership/repository/SharedFundRepository.java
@@ -1,9 +1,14 @@
 package com.group8.evcoownership.repository;
 
 import com.group8.evcoownership.entity.SharedFund;
+import com.group8.evcoownership.enums.FundType;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,4 +20,14 @@ public interface SharedFundRepository extends JpaRepository<SharedFund, Long> {
     Page<SharedFund> findAllByGroup_GroupId(Long groupId, Pageable pageable);
 
     boolean existsByGroup_GroupId(long group_GroupId);
+
+    // New for Fund Repository
+    Optional<SharedFund> findByGroup_GroupIdAndFundType(Long groupId, FundType type);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select f from SharedFund f where f.group.groupId=:gid and f.fundType=:type")
+    Optional<SharedFund> lockByGroupAndType(@Param("gid") Long gid, @Param("type") FundType type);
+
+    boolean existsByGroup_GroupIdAndFundType(Long groupId, FundType type);
+
 }
