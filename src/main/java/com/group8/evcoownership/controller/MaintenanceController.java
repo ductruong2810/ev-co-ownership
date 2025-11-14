71be1732c238a9682021f750be1fe4c574ae3ba5
khanhvy0908 <khanhvy0946265560@gmail.com>
2025-11-14 15:10:28 +0700

    fix MaintenanceController

--- src/main/java/com/group8/evcoownership/controller/MaintenanceController.java
+++ src/main/java/com/group8/evcoownership/controller/MaintenanceController.java
@@ -49,100 +49,102 @@ public class MaintenanceController {
         return ResponseEntity.ok(maintenanceService.create(req, auth.getName()));
     }
 
-//    // ==================== UPDATE ==========================
-//    @PutMapping("/{id}")
-//    @PreAuthorize("hasAnyRole('TECHNICIAN', 'STAFF', 'ADMIN')")
-//    @Operation(summary = "[Technician]Cập nhật yêu cầu bảo trì",
-//            description = "Chỉ cho phép cập nhật khi trạng thái là PENDING. Cho phép TECHNICIAN, STAFF, ADMIN.")
-//    public ResponseEntity<MaintenanceResponseDTO> update(
-//            @PathVariable Long id,
-//            @Valid @RequestBody MaintenanceUpdateRequestDTO req,
-//            Authentication auth
-//    ) {
-//        return ResponseEntity.ok(maintenanceService.update(id, req, auth.getName()));
-//    }
-//
-//    // ==================== Technician get his Maintenance requests =============
-//    @GetMapping("/my-requests")
-//    @PreAuthorize("hasAnyRole('TECHNICIAN', 'STAFF', 'ADMIN')")
-//    @Operation(summary = "[Technician] Xem yêu cầu bảo trì của chính mình",
-//            description = "Dành cho technician xem lại các yêu cầu bảo trì do họ đã tạo.")
-//    public ResponseEntity<List<MaintenanceResponseDTO>> getMyRequests(Authentication auth) {
-//        return ResponseEntity.ok(maintenanceService.getMyRequests(auth.getName()));
-//    }
-//
-//
-//
-//    // ======================================================
-//    // ===================== STAFF / ADMIN ==================
-//    // ======================================================
-//
-//    /**
-//     *  Xem danh sách tất cả yêu cầu bảo trì
-//     */
-//    @GetMapping
-//    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
-//    @Operation(summary = "[Staff/Admin] Danh sách yêu cầu bảo trì", description = "Hiển thị danh sách toàn bộ các yêu cầu bảo trì hiện có trong hệ thống.")
-//    public ResponseEntity<List<MaintenanceResponseDTO>> getAll() {
-//        return ResponseEntity.ok(maintenanceService.getAll());
-//    }
-//
-//    /**
-//     *  Duyệt yêu cầu bảo trì (PENDING → APPROVED)
-//     */
-//    @PutMapping("/{id}/approve")
-//    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
-//    @Operation(
-//            summary = "[Staff/Admin] Duyệt yêu cầu bảo trì",
-//            description = """
-//                    Nhân viên hoặc quản trị viên duyệt yêu cầu bảo trì hợp lý → chuyển trạng thái từ PENDING sang APPROVED.
-//                    - Ghi nhận người duyệt.
-//                    - Trigger Expense tự động (trừ quỹ nhóm).
-//                    """
-//    )
-//    public ResponseEntity<MaintenanceResponseDTO> approve(
-//            @PathVariable Long id,
-//            @RequestParam LocalDate nextDueDate, // staff nhập ngày kế tiếp
-//            Authentication auth
-//    ) {
-//        return ResponseEntity.ok(maintenanceService.approve(id, auth.getName(), nextDueDate));
-//    }
-//
-//    /**
-//     *  Từ chối yêu cầu bảo trì (PENDING → REJECTED)
-//     */
-//    @PutMapping("/{id}/reject")
-//    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
-//    @Operation(
-//            summary = "[Staff/Admin] Từ chối yêu cầu bảo trì",
-//            description = """
-//                    Nhân viên hoặc quản trị viên xác định yêu cầu không hợp lệ → chuyển trạng thái từ PENDING sang REJECTED.
-//                    - Ghi nhận người từ chối.
-//                    """
-//    )
-//    public ResponseEntity<MaintenanceResponseDTO> reject(
-//            @PathVariable Long id,
-//            Authentication auth
-//    ) {
-//        return ResponseEntity.ok(maintenanceService.reject(id, auth.getName()));
-//    }
-//
-//    /**
-//     * Xem chi tiết yêu cầu bảo trì
-//     */
-//    @GetMapping("/{id}")
-//    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
-//    @Operation(
-//            summary = "[Staff/Admin] Xem chi tiết yêu cầu bảo trì",
-//            description = """
-//            Staff và Admin đều có thể xem chi tiết yêu cầu bảo trì để kiểm tra thông tin trước khi duyệt hoặc từ chối.
-//            Admin có thể xem toàn bộ; Staff chỉ xem trong phạm vi nhóm mà họ quản lý.
-//            """
-//    )
-//    public ResponseEntity<MaintenanceResponseDTO> getOne(
-//            @PathVariable Long id
-//    ) {
-//        return ResponseEntity.ok(maintenanceService.getOne(id));
-//    }
+    // ==================== UPDATE ==========================
+    // Technician cap nhat yeu cau khi van con trang thai Pending
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('TECHNICIAN')")
+    @Operation(summary = "[Technician]Cập nhật yêu cầu bảo trì",
+            description = "Chỉ cho phép cập nhật khi trạng thái là PENDING. Cho phép TECHNICIAN, STAFF, ADMIN.")
+    public ResponseEntity<MaintenanceResponseDTO> update(
+            @PathVariable Long id,
+            @Valid @RequestBody MaintenanceUpdateRequestDTO req,
+            Authentication auth
+    ) {
+        return ResponseEntity.ok(maintenanceService.update(id, req, auth.getName()));
+    }
+
+    // ==================== Technician get his Maintenance requests =============
+    // Technician xem yeu cau bao tri cua minh
+    @GetMapping("/my-requests")
+    @PreAuthorize("hasAnyRole('TECHNICIAN')")
+    @Operation(summary = "[Technician] Xem yêu cầu bảo trì của chính mình",
+            description = "Dành cho technician xem lại các yêu cầu bảo trì do họ đã tạo.")
+    public ResponseEntity<List<MaintenanceResponseDTO>> getMyRequests(Authentication auth) {
+        return ResponseEntity.ok(maintenanceService.getMyRequests(auth.getName()));
+    }
+
+
+
+    // ======================================================
+    // ===================== STAFF / ADMIN ==================
+    // ======================================================
+
+    /**
+     *  Staff/Admin Xem danh sách tất cả yêu cầu bảo trì
+     */
+    @GetMapping
+    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
+    @Operation(summary = "[Staff/Admin] Danh sách yêu cầu bảo trì", description = "Hiển thị danh sách toàn bộ các yêu cầu bảo trì hiện có trong hệ thống.")
+    public ResponseEntity<List<MaintenanceResponseDTO>> getAll() {
+        return ResponseEntity.ok(maintenanceService.getAll());
+    }
+
+    /**
+     *  Staff/Admin Duyệt yêu cầu bảo trì (PENDING → APPROVED)
+     */
+    @PutMapping("/{id}/approve")
+    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
+    @Operation(
+            summary = "[Staff/Admin] Duyệt yêu cầu bảo trì",
+            description = """
+                    Nhân viên hoặc quản trị viên duyệt yêu cầu bảo trì hợp lý → chuyển trạng thái từ PENDING sang APPROVED.
+                    - Ghi nhận người duyệt.
+                    - Trigger Expense tự động (trừ quỹ nhóm).
+                    """
+    )
+    public ResponseEntity<MaintenanceResponseDTO> approve(
+            @PathVariable Long id,
+            @RequestParam LocalDate nextDueDate, // staff nhập ngày kế tiếp
+            Authentication auth
+    ) {
+        return ResponseEntity.ok(maintenanceService.approve(id, auth.getName(), nextDueDate));
+    }
+
+    /**
+     *  Staff/Admin Từ chối yêu cầu bảo trì (PENDING → REJECTED)
+     */
+    @PutMapping("/{id}/reject")
+    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
+    @Operation(
+            summary = "[Staff/Admin] Từ chối yêu cầu bảo trì",
+            description = """
+                    Nhân viên hoặc quản trị viên xác định yêu cầu không hợp lệ → chuyển trạng thái từ PENDING sang REJECTED.
+                    - Ghi nhận người từ chối.
+                    """
+    )
+    public ResponseEntity<MaintenanceResponseDTO> reject(
+            @PathVariable Long id,
+            Authentication auth
+    ) {
+        return ResponseEntity.ok(maintenanceService.reject(id, auth.getName()));
+    }
+
+    /**
+     * Staff va Admin Xem chi tiết yêu cầu bảo trì
+     */
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('STAFF', 'ADMIN')")
+    @Operation(
+            summary = "[Staff/Admin] Xem chi tiết yêu cầu bảo trì",
+            description = """
+            Staff và Admin đều có thể xem chi tiết yêu cầu bảo trì để kiểm tra thông tin trước khi duyệt hoặc từ chối.
+            Admin có thể xem toàn bộ; Staff chỉ xem trong phạm vi nhóm mà họ quản lý.
+            """
+    )
+    public ResponseEntity<MaintenanceResponseDTO> getOne(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(maintenanceService.getOne(id));
+    }
 
 }
