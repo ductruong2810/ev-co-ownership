32e5959b3a445cd9470c39a4a23ce2e69e4c798b
truonghoang <boybybame@gmail.com>
2025-12-01 02:09:11 +0700

    Remove unused import DisputeType from DisputeRepository

--- src/main/java/com/group8/evcoownership/repository/DisputeRepository.java
+++ src/main/java/com/group8/evcoownership/repository/DisputeRepository.java
@@ -1,8 +1,6 @@
 package com.group8.evcoownership.repository;
 
 import com.group8.evcoownership.entity.Dispute;
-import com.group8.evcoownership.enums.DisputeStatus;
-import com.group8.evcoownership.enums.DisputeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
